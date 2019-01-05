9c4fe66a16e9f140b56478da3f41606bce74a0a3
monzo <monzo@LAPTOP-PA8FMJ55>
2019-01-05 13:42:08 +0100

    provant git

--- treball_EDD/src/treball_EDD/Practica32.java
+++ treball_EDD/src/treball_EDD/Practica32.java
@@ -3,8 +3,9 @@ package treball_EDD;
 public class Practica32 {
 	public static void main(String[] args) {
 		
-		System.out.println("Bon dia món");
+		System.out.println("Bon dia mon");
 		System.out.println("Bon dia classe");
+		System.out.println("Provant Git");
 	}
 
 }
